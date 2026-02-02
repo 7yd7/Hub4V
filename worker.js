efb50b5031780a633f6cb4c592577bcd34105f75
7yd7 <110498114+7yd7@users.noreply.github.com>
2026-02-02 04:54:20 +0300

    Delete worker.js

--- worker.js
+++
@@ -1,7 +0,0 @@
-export default {
-    async fetch(request) {
-        return new Response('loadstring(game:HttpGet("https://raw.githubusercontent.com/7yd7/Hub4V/Menu/client.luau"))()', {
-            headers: { "content-type": "text/plain" },
-        });
-    },
-};
